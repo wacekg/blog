8263458d923b700249751c75e325bd40ed8381ea
Tomasz Szkaradek <tomasz.szkaradek@codesthq.com>
2017-12-02 13:32:34 +0100

    Add show method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,6 +9,10 @@ class ArticlesController < ApplicationController
     article_params = params.require(:article).permit(:title, :text)
     @article = Article.new(article_params)
     @article.save
-    redirect_to articles_path
+    redirect_to article_path(@article)
+  end
+
+  def show
+    @article = Article.find(params[:id])
   end
 end
