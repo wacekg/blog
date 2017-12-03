18c6a1835791fd2a1e5cdce3636a14a009abe56b
Tomasz Szkaradek <tomasz.szkaradek@codesthq.com>
2017-12-03 10:33:23 +0100

    Add destroy option for article

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
   get 'articles/:id', to: 'articles#show', as: 'article'
   get 'articles/:id/edit', to: 'articles#edit', as: 'edit_article'
   patch 'articles/:id', to: 'articles#update'
+  delete 'articles/:id', to: 'articles#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
